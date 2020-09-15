4d1d2eec50d977ed12db2003e46fe94b40071ac7
Tomonori HIRATA <hirata@sqlab.jp>
2020-09-15 22:47:49 +0900

    update: Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import MainContainer from './Components/MainContainer'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <MainContainer />
-      </div>
+      <Router>
+        <Route exact path="/" component={MainContainer}></Route>
+        <Route exact path="/sdf" component={MainContainer}></Route>
+      </Router>
     );
   }
 }
