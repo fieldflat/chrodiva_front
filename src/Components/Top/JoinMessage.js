9967fe7783ffa1a4763e2d262f24d0c81ab38665
Tomonori HIRATA <hirata@sqlab.jp>
2020-09-17 00:03:57 +0900

    update: Linkを導入

--- src/Components/Top/JoinMessage.js
+++ src/Components/Top/JoinMessage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import join_message from "../../css/top/join_message.module.css";
 import top_message from "../../css/top/top_message.module.css";
 import container from "../../css/basis/container.module.css";
@@ -12,7 +13,7 @@ export default function JoinMessage(props) {
         <button id={top_message.get_started_button}>無料で始める！</button>
         <p></p>
         <span id={top_message.login_message}>
-          ユーザ登録がお済みの方は<a href="#">こちら</a>へ
+          ユーザ登録がお済みの方は<Link to="/login">こちら</Link>へ
         </span>
       </div>
     </div>
